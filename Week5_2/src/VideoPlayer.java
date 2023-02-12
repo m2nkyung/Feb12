e9f0b535a829c284519604a2ceb908283b1a38ce
jerrx18 <happyjerrychristmas@gmail.com>
2023-02-12 21:30:24 +0900

    commit

---
+++ Week5_2/src/VideoPlayer.java
@@ -0,0 +1,14 @@
+public class VideoPlayer implements Controllable {
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+		System.out.println("PLAY");
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		System.out.println("STOP");
+	}
+}
---
+++ Week5_2/src/Week5_2.java
@@ -0,0 +1,11 @@
+//무명 내부 클래스를 이용하여 p296  #3 을 완성하시오.
+
+public class Week5_2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		VideoPlayer vp = new VideoPlayer();
+		vp.play();
+		vp.stop();
+	}
+}
